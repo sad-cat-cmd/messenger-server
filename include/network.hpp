fb5a39656e9bde16434b3a430772541304704727
vadim <vadim.polinevskiy06@mail.ru>
2026-09-12 22:47:04 +0300

    feat(): add new namespace transmitted_objects

--- include/network.hpp
+++ include/network.hpp
@@ -13,6 +13,40 @@
 #include <atomic>
 
 namespace network {
+
+    namespace transmitted_objects {
+        const size_t MAX_BYTE_TEXT_MSG = 2000;
+        const size_t MAX_BYTE_FILE_NAME = 100;
+
+        constexpr size_t MAX_SIZE_BYTE_FILE = 1024 * 1024 * 64;
+        constexpr size_t SIZE_BYTE_FILE_CHUNK = 1024 * 64;
+
+        class RequestSendText {
+            private:
+            std::string idSender;
+            std::string idRecver;
+            std::string text;
+        };
+        class RequestSendFile {
+            private:
+            std::string chunk;
+            std::string fileName;
+            std::string fileSender;
+            std::string fileRecver;
+        };
+        class RequestRecvText {
+            private:
+            std::
+        };
+        class RequestRecvFile;
+        class Text;
+        class Chat;
+        class Count;
+        class FileChunk;
+        class ClientInfo;
+        class LogINInfo;
+        class StopProcess;
+    }
     class Socket {
         private:
         std::atomic<int> numberSocket;
@@ -28,5 +62,21 @@ namespace network {
         bool isLessZero() noexcept;
         bool isClosed() noexcept;
         void closeSocket() noexcept;
+    };
+
+    class IClient {
+        protected:
+        QString name;
+        QString id;
+        bool status;
+
+
+    };
+    class ServerThreadAccept : public QThread {
+        private:
+        network::Socket & lserverSocket;
+
+        ServerThreadAccept(network::Socket & serverSocket);
+
     };
 }
